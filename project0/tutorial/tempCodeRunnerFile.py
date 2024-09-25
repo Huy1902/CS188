ff3a1b02636243bda32eecc04da9c1bdd3a04b3e
Huy1902 <lymstar19@gmail.com>
2024-09-25 20:22:59 +0700

    Create git for the project I made in CSS188.

---
+++ project0/tutorial/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+    dir1 = {'apples': 2.0, 'oranges':1.0}
+    shop1 =  shop.FruitShop('shop1',dir1)
+    dir2 = {'apples': 1.0, 'oranges': 5.0}
+    shop2 = shop.FruitShop('shop2',dir2)
+    shops = [shop1, shop2]
+
+    order = [('apples',3.0)]
