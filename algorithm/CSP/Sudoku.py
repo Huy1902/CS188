b2b43c357123974bcc8fe02d5012ef2fb7dc4ad6
Huy1902 <lymstar19@gmail.com>
2024-10-12 16:37:10 +0700

    Optimize Sudoku solver with filtering and ordering
    Implemented forward checking and arc consistency. Added the least constraining value heuristic for better variable ordering. These improvements enhance the solver's efficiency and reduce the number of assignments.

--- algorithm/CSP/Sudoku.py
+++ algorithm/CSP/Sudoku.py
@@ -1,3 +1,6 @@
+from os import remove
+from queue import Queue
+
 puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
           [6, 0, 0, 1, 9, 5, 0, 0, 0],
           [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -31,6 +34,7 @@ class CSP:
         self.domains = domains
         self.constraints = constrains
         self.count_assignment = 0
+        self.filtering()
 
     def solve(self):
         assignments = {}
@@ -41,19 +45,75 @@ class CSP:
         if len(assignments) == len(self.variables):
             return assignments
         variable = self.get_most_constrained_variable(assignments)
-        for value in self.domains[variable]:
-            if self.is_consistent(variable, value, assignments):
-                self.count_assignment += 1
-                assignments[variable] = value
-                next_assignments = self.backtracking(assignments)
-                if next_assignments is not None:
-                    return next_assignments
-                del assignments[variable]
+        consistent_value = [value for value in self.domains[variable] if
+                            self.is_consistent(variable, value, assignments)]
+
+        while consistent_value:
+            self.count_assignment += 1
+            assignments[variable] = self.get_least_constraining_value(variable, consistent_value, assignments)
+            next_assignments = self.backtracking(assignments)
+            if next_assignments is not None:
+                return next_assignments
+            del consistent_value[consistent_value.index(assignments[variable])]
+            del assignments[variable]
+
+        # for value in consistent_value:
+        #     self.count_assignment += 1
+        #     assignments[variable] = value
+        #     next_assignments = self.backtracking(assignments)
+        #     if next_assignments is not None:
+        #         return next_assignments
+        #     del assignments[variable]
         return None
 
+    # Optimization: Filtering: Forward checking & Arc consistency
+    def filtering(self) -> None:
+        queue = Queue()
+        for i in range(9):
+            for j in range(9):
+                for constraint in self.constraints[(i, j)]:
+                    queue.put((constraint, (i, j)))
+        while not queue.empty():
+            constraint = queue.get()
+            head = constraint[0]
+            tail = constraint[1]
+            if self.remove_inconsistent_value(tail, head):
+                for constraint in self.constraints[tail]:
+                    queue.put((tail, constraint))
+
+
+    def remove_inconsistent_value(self, tail, head):
+        removed = False
+        for x in self.domains[tail]:
+            exist_y_satisfy_constraint = False
+            for y in self.domains[head]:
+                if y != x:
+                    exist_y_satisfy_constraint = True
+                    break
+            if not exist_y_satisfy_constraint:
+                self.domains[tail].remove(x)
+                removed = True
+        return removed
+
+
+    # Optimization: Ordering: The Minimum Remaining Values
     def get_most_constrained_variable(self, assignments):
         unassigned_var = [var for var in self.variables if var not in assignments.keys()]
         return min(unassigned_var, key=lambda var: len(self.domains[var]))
+
+
+    def count_constrain_of_value(self, remaining_var, value):
+        count = 0
+        for var in remaining_var:
+            if value in self.domains[var]:
+                count += 1
+        return count
+
+    # Optimization: Ordering: The Least Constraining Value
+    def get_least_constraining_value(self, variable, domain, assignments):
+        remaining_var = [var for var in self.constraints[variable] if var not in assignments.keys()]
+        return min(domain, key=lambda value: self.count_constrain_of_value(remaining_var, value))
+
 
     def is_consistent(self, variable, value, assignments: dict):
         for constraint in self.constraints[variable]:
@@ -86,8 +146,8 @@ def add_constrain(var: tuple, constraints: dict):
 
 variables = [(i, j) for i in range(9) for j in range(9)]
 
-domains = {var: set(range(1, 10)) if puzzle[var[0]][var[1]] == 0
-else {puzzle[var[0]][var[1]]} for var in variables}
+domains = {var: list(range(1, 10)) if puzzle[var[0]][var[1]] == 0
+else [puzzle[var[0]][var[1]]] for var in variables}
 
 constraints = {}
 for i in range(9):
@@ -100,5 +160,5 @@ puzzle_solution = [[0 for j in range(9)] for i in range(9)]
 for i in range(9):
     for j in range(9):
         puzzle_solution[i][j] = solution[(i, j)]
-print_sudoku(puzzle_solution)
 print(CSP_problem.count_assignment)
+print_sudoku(puzzle_solution)
